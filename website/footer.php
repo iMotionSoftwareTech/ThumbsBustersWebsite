255e7153af2d14bd55f60962d14984f6037cf6c1
iMotionSoftwareTech <imotionsoftware@hotmail.com>
2026-07-25 04:17:03 +0100

    Closes #3 - Partial PHP Views
    Closes #3 - Created partial php pages that contained bits of HTML that were incorporated into the main index.php page.

---
+++ website/footer.php
@@ -0,0 +1,34 @@
+<footer class="footer">
+
+    <div class="footer-copyright">
+        <p>&copy; Thumb-Busters 2026</p>
+    </div>
+
+    <div class="footer-column">
+        <a href="#">About Us</a>
+        <a href="#">Contact Us</a>
+        <a href="#">FAQs</a>
+    </div>
+
+    <div class="footer-column">
+        <a href="#">Stores</a>
+        <a href="#">Returns</a>
+        <a href="#">Deliveries</a>
+    </div>
+
+    <div class="footer-column">
+        <a href="#">Terms and Conditions</a>
+        <a href="#">Privacy Policy</a>
+    </div>
+
+    <div class="footer-social">
+        <p>Follow Us On</p>
+
+        <div class="social-icons">
+            <a href="#" aria-label="Visit our Facebook page"><i class="fab fa-facebook"></i></a>
+            <a href="#" aria-label="Visit our X page"><i class="fab fa-x-twitter"></i></a>
+            <a href="#" aria-label="Visit our Instagram page"><i class="fab fa-instagram"></i></a>
+        </div>
+    </div>
+
+</footer>
